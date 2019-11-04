0c89b87a4091b4a2a5deb7989f81656b1e826c0f
Marco Antonio Dominguez <madmzr2gmail.com>
2019-11-04 08:35:32 -0500

    Update safety check for masonry grid, lazy evaluation to ensure images are loaded

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+import './polyfill/array-from';
 import Masonry from 'masonry-layout';
 import 'waypoints/lib/noframework.waypoints.js'
 
@@ -8,10 +9,22 @@ const home = document.querySelector('#home')
 
 home.style.height = `${windowHeight > MAX_HEIGHT ? MAX_HEIGHT : windowHeight}px`
 
-const grids = Array.from(document.querySelectorAll('.img-grid')).map((grid) => (
-	new Masonry(grid, {
- 	  itemSelector: '.grid-item',
- 	  percentPosition: true,
- 	  columnWidth: '.grid-sizer'
-	})
-))
+function generateGrids() {
+	return Array.from(document.querySelectorAll('.img-grid')).map((grid) => (
+		new Masonry(grid, {
+	 	  itemSelector: '.grid-item',
+	 	  percentPosition: true,
+	 	  columnWidth: '.grid-sizer'
+		})
+	))
+}
+
+const imgs = Array.from(document.querySelectorAll('img'))
+let interalForLogImg;
+
+interalForLogImg = setInterval(() => {
+	if (imgs.find(img => !img.src)) return
+
+	clearInterval(interalForLogImg)
+  generateGrids()
+}, 1500)
