b71f9dba6faf74ca1ceda81c1a5f22b74cb27de4
Marco Antonio Dominguez <madmzr2gmail.com>
2019-11-04 00:03:28 -0500

    Adding draft

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+
+import Masonry from 'masonry-layout';
+import 'waypoints/lib/noframework.waypoints.js'
+
+const MAX_HEIGHT = 1080
+
+const windowHeight = window.innerHeight || document.document.documentElement.clientHeight || document.body.clientHeight
+const home = document.querySelector('#home')
+
+home.style.height = `${windowHeight > MAX_HEIGHT ? MAX_HEIGHT : windowHeight}px`
+
+const grids = Array.from(document.querySelectorAll('.img-grid')).map((grid) => (
+	new Masonry(grid, {
+ 	  itemSelector: '.grid-item',
+ 	  percentPosition: true,
+ 	  columnWidth: '.grid-sizer'
+	})
+))
+
+grids.forEach(grid => {
+	console.log(grid, grid.imagesLoaded)
+})
