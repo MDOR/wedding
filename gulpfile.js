e93c2c421ecb73f94875a72ed9b9f2d6914546dd
Marco Antonio Dominguez <madmzr2gmail.com>
2019-11-04 00:02:51 -0500

    Adding configuration files

---
+++ gulpfile.js
@@ -0,0 +1,108 @@
+const path = require('path')
+const del = require('del')
+const browserify = require('browserify');
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const babel = require('gulp-babel')
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat')
+const copy = require('gulp-copy')
+const browserSync = require('browser-sync')
+
+const server = browserSync.create();
+
+const paths = {
+  scripts: {
+    src: ['src/js/*.js', 'src/js/**/*.js'],
+    dest: 'docs/js/'
+  },
+   styles: {
+    src: ['src/scss/*.scss', 'src/scss/**/*.scss'],
+    dest: 'docs/css/'
+  },
+  html: {
+    src: ['src/*.html'],
+    dest: 'docs/'
+  },
+  img: {
+    src: ['src/img/*.{jpg,jpeg,png,svg}', 'src/img/**/*.{jpg,jpeg,png,svg}'],
+    dest: 'docs/img'
+  }
+};
+
+const clean = () => del(['docs']);
+
+function scripts() {
+  const b = browserify({
+    entries: './src/js/main.js',
+    debug: true
+  })
+  .transform("babelify");
+
+  return b.bundle()
+    .pipe(source('main.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        .on('error', (e) => console.log(require('util').inspect(e)))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./docs/js/'));
+
+  return gulp.src(paths.scripts.src, { sourcemaps: true })
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(concat('main.min.js'))
+    .pipe(gulp.dest(paths.scripts.dest));
+}
+
+function styles() {
+  return gulp.src(paths.styles.src, { sourcemaps: true })
+    .pipe(sass({
+    	includePaths: [
+    		 path.join(__dirname, '/node_modules/normalize-scss/sass/'),
+    	]
+    }).on('error', sass.logError))
+    .pipe(gulp.dest(paths.styles.dest));
+}
+
+function html() {
+  return gulp.src(paths.html.src)
+    .pipe(gulp.dest(paths.html.dest));
+}
+
+function img() {
+  return gulp.src(paths.img.src)
+    .pipe(gulp.dest(paths.img.dest));
+}
+
+function reload(done) {
+  server.reload();
+  done();
+}
+
+function serve(done) {
+  server.init({
+    server: {
+      baseDir: './docs'
+    }
+  });
+  done();
+}
+
+const watch = () => {
+	gulp.series(clean, scripts, styles, img, html);
+	gulp.watch(paths.scripts.src, gulp.series(scripts, reload))
+	gulp.watch(paths.styles.src, gulp.series(styles, reload))
+	gulp.watch(paths.img.src, gulp.series(img, reload))
+	gulp.watch(paths.html.src, gulp.series(html, reload))
+};
+
+const dev = gulp.series(clean, scripts,styles, html, img, serve, watch);
+
+gulp.task('default', dev)
+
+module.exports = dev;
