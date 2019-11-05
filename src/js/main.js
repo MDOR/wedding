5d43231db67d3c87f59da9a16c09bc99f340c649
Marco Antonio Dominguez <madmzr2gmail.com>
2019-11-05 07:51:07 -0500

    Update content

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,30 @@
 import './polyfill/array-from';
 import Masonry from 'masonry-layout';
-import 'waypoints/lib/noframework.waypoints.js'
+import 'waypoints/lib/noframework.waypoints.js';
 
 const MAX_HEIGHT = 1080
-
-const windowHeight = window.innerHeight || document.document.documentElement.clientHeight || document.body.clientHeight
 const home = document.querySelector('#home')
+const imgs = Array.from(document.querySelectorAll('img'))
+const body = document.body || document.documentElement;
 
-home.style.height = `${windowHeight > MAX_HEIGHT ? MAX_HEIGHT : windowHeight}px`
+const  debouncer = (fn, time = 100) => {
+  let timeout
+
+  return function debouncedFn(...args) {
+  	clearTimeout(timeout);
+  	timeout = setTimeout(() => (timeout = null, fn(...args)), time)
+  }
+}
+
+function setHomeSize() {
+  const windowHeight = window.innerHeight || document.document.documentElement.clientHeight || document.body.clientHeight
+  home.style.height = `${windowHeight > MAX_HEIGHT ? MAX_HEIGHT : windowHeight}px`
+}
+
+const debouncedSetHome = debouncer(setHomeSize);
+/* set home size */
+(setHomeSize)();
+window.addEventListener('resize', debouncedSetHome);
 
 function generateGrids() {
 	return Array.from(document.querySelectorAll('.img-grid')).map((grid) => (
@@ -19,12 +36,55 @@ function generateGrids() {
 	))
 }
 
-const imgs = Array.from(document.querySelectorAll('img'))
 let interalForLogImg;
-
 interalForLogImg = setInterval(() => {
 	if (imgs.find(img => !img.src)) return
 
 	clearInterval(interalForLogImg)
   generateGrids()
 }, 1500)
+
+const getCurrentScroll = () => document.scrollTop  || document.documentElement.scrollTop  || document.body.scrollTop
+
+function scrollToUtil (target = 0) {
+	const beginning = getCurrentScroll()
+	const increment =  (target - beginning) / 100
+	const initial = target;
+	let timeout;
+	function scroll() {
+		const current = getCurrentScroll()
+		window.scrollTo(0, current + increment)
+
+		if (
+			current === target ||
+			(initial > beginning && target < current) ||
+			(initial < beginning && target > current)
+		) return
+
+		timeout = setTimeout(() => window.requestAnimationFrame(scroll), 10)
+	}
+
+	const cancel = () => console.log('cancel') ||(
+    clearTimeout(timeout),
+    window.cancelAnimationFrame(scroll)
+	)
+
+	window.addEventListener('scroll', cancel);
+
+	window.requestAnimationFrame(scroll)
+}
+
+const move = document.querySelectorAll('[data-move-to]');
+
+Array.from(move).forEach((item) => {
+	function moveTo (e) {
+    e.preventDefault()
+    const to = e.currentTarget.dataset.moveTo
+    const target = document.querySelector(to)
+    console.log(to, target)
+    console.log(target.scrollHeight)
+    scrollToUtil(target.scrollHeight)
+	}
+
+	item.addEventListener('click', moveTo)
+})
