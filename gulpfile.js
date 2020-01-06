4c57b8827bfbed5a97aff9df3fb6379a83ee03ed
Marco Antonio Dominguez <madmzr2gmail.com>
2020-01-05 22:16:01 -0500

    Include husky and lint-staged

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
-const path = require('path')
-const del = require('del')
+const path = require('path');
+const del = require('del');
 const browserify = require('browserify');
-const gulp = require('gulp')
-const sass = require('gulp-sass')
-const babel = require('gulp-babel')
-const source = require('vinyl-source-stream')
-const buffer = require('vinyl-buffer')
-const uglify = require('gulp-uglify')
-const sourcemaps = require('gulp-sourcemaps')
-const concat = require('gulp-concat')
-const copy = require('gulp-copy')
-const purge = require('gulp-css-purge')
-const cleanCSS = require('gulp-clean-css')
-const imagemin = require('gulp-imagemin')
-const htmlmin = require('gulp-htmlmin')
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const copy = require('gulp-copy');
+const purge = require('gulp-css-purge');
+const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 const imageminGuetzli = require('imagemin-guetzli');
-const browserSync = require('browser-sync')
+const browserSync = require('browser-sync');
 
 const server = browserSync.create();
 
@@ -24,7 +24,7 @@ const paths = {
     src: ['src/js/*.js', 'src/js/**/*.js'],
     dest: 'docs/js/'
   },
-   styles: {
+  styles: {
     src: ['src/scss/*.scss', 'src/scss/**/*.scss'],
     dest: 'docs/css/'
   },
@@ -33,98 +33,111 @@ const paths = {
     dest: 'docs/'
   },
   img: {
-    src: ['src/img/*.{png,svg,ico,webp,gif}', 'src/img/**/*.{png,svg,ico,webp,gif}'],
+    src: [
+      'src/img/*.{png,svg,ico,webp,gif}',
+      'src/img/**/*.{png,svg,ico,webp,gif}'
+    ],
     dest: 'docs/img'
   },
   imgJPG: {
     src: ['src/img/*.{jpg,jpeg}', 'src/img/**/*.{jpg,jpeg}'],
     dest: 'docs/img'
   }
-}
+};
 
-const clean = () => del(['docs'])
+const clean = () => del(['docs']);
 
 function scripts() {
   const b = browserify({
     entries: './src/js/main.js',
     debug: true
-  })
-  .transform("babelify");
+  }).transform('babelify');
 
-  return b.bundle()
-    .pipe(source('main.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-        // Add transformation tasks to the pipeline here.
-        .pipe(uglify())
-        .on('error', (e) => console.log(require('util').inspect(e)))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./docs/js/'))
+  return (
+    b
+      .bundle()
+      .pipe(source('main.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      // Add transformation tasks to the pipeline here.
+      .pipe(uglify())
+      .on('error', e => console.log(require('util').inspect(e)))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./docs/js/'))
+  );
 
-  return gulp.src(paths.scripts.src, { sourcemaps: true })
+  return gulp
+    .src(paths.scripts.src, { sourcemaps: true })
     .pipe(babel())
     .pipe(uglify())
     .pipe(concat('main.min.js'))
-    .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(gulp.dest(paths.scripts.dest));
 }
 
 function styles() {
-  return gulp.src(paths.styles.src, { sourcemaps: true })
-    .pipe(sass({
-    	includePaths: [
-    		 path.join(__dirname, '/node_modules/normalize-scss/sass/'),
-    	]
-    }).on('error', sass.logError))
-    .pipe(purge({
-      trim : true,
-      shorten : true,
-      verbose : true
-    }))
+  return gulp
+    .src(paths.styles.src, { sourcemaps: true })
+    .pipe(
+      sass({
+        includePaths: [
+          path.join(__dirname, '/node_modules/normalize-scss/sass/')
+        ]
+      }).on('error', sass.logError)
+    )
+    .pipe(
+      purge({
+        trim: true,
+        shorten: true,
+        verbose: true
+      })
+    )
     .pipe(cleanCSS())
-    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(gulp.dest(paths.styles.dest));
 }
 
 function html() {
-  return gulp.src(paths.html.src)
-    .pipe(htmlmin({
-    	collapseWhitespace: true,
-    	removeComments: true
-    }))
-    .pipe(gulp.dest(paths.html.dest))
+  return gulp
+    .src(paths.html.src)
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+        removeComments: true
+      })
+    )
+    .pipe(gulp.dest(paths.html.dest));
 }
 
 function img(done) {
-  gulp.src(paths.img.src)
-		.pipe(imagemin([
-			imagemin.optipng({interlaced: true
-			}),
-			imagemin.gifsicle({interlaced: true}),
-	    imagemin.svgo({})
-		]))
-    .pipe(gulp.dest(paths.img.dest))
+  gulp
+    .src(paths.img.src)
+    .pipe(
+      imagemin([
+        imagemin.optipng({ interlaced: true }),
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.svgo({})
+      ])
+    )
+    .pipe(gulp.dest(paths.img.dest));
 
-  gulp.src(paths.imgJPG.src)
-		.pipe(imagemin([
-			imageminGuetzli({quality: 85}),
-		]))
-    .pipe(gulp.dest(paths.imgJPG.dest))
+  gulp
+    .src(paths.imgJPG.src)
+    .pipe(imagemin([imageminGuetzli({ quality: 85 })]))
+    .pipe(gulp.dest(paths.imgJPG.dest));
 
-  done()
+  done();
 }
 
 function imgCopy(done) {
-  gulp.src(paths.img.src)
-    .pipe(gulp.dest(paths.img.dest))
+  gulp.src(paths.img.src).pipe(gulp.dest(paths.img.dest));
 
-  gulp.src(paths.imgJPG.src)
-    .pipe(gulp.dest(paths.imgJPG.dest))
+  gulp.src(paths.imgJPG.src).pipe(gulp.dest(paths.imgJPG.dest));
 
-  done()
+  done();
 }
 
 function reload(done) {
-  server.reload()
-  done()
+  server.reload();
+  done();
 }
 
 function serve(done) {
@@ -133,16 +146,22 @@ function serve(done) {
       baseDir: './docs'
     }
   });
-  done()
+  done();
 }
 
 const watch = () => {
-	gulp.series(clean, scripts, styles, imgCopy, html);
-	gulp.watch(paths.scripts.src, gulp.series(scripts, reload))
-	gulp.watch(paths.styles.src, gulp.series(styles, reload))
-	gulp.watch(paths.img.src.concat(paths.imgJPG.src), gulp.series(imgCopy, reload))
-	gulp.watch(paths.html.src, gulp.series(html, reload))
+  gulp.series(clean, scripts, styles, imgCopy, html);
+  gulp.watch(paths.scripts.src, gulp.series(scripts, reload));
+  gulp.watch(paths.styles.src, gulp.series(styles, reload));
+  gulp.watch(
+    paths.img.src.concat(paths.imgJPG.src),
+    gulp.series(imgCopy, reload)
+  );
+  gulp.watch(paths.html.src, gulp.series(html, reload));
 };
 
-gulp.task('default', gulp.series(clean, scripts,styles, html, img))
-gulp.task('watch', gulp.series(clean, scripts,styles, html, imgCopy, serve, watch))
+gulp.task('default', gulp.series(clean, scripts, styles, html, img));
+gulp.task(
+  'watch',
+  gulp.series(clean, scripts, styles, html, imgCopy, serve, watch)
+);
